bcbdbe721e218fbea7ccdd9d740ad5d2a7efd979
frostmeta <calvin.hobo@gmail.com>
2022-02-24 18:03:57 -0500

    Artisan creation of customers table
    Added in some basic customer fields in migrations table

---
+++ backend/database/migrations/2022_02_24_210720_create_customers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Faker\Provider\bg_BG\PhoneNumber;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('phone_number');
+            $table->unsignedBigInteger('order_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
