76f0ae37b58566ae2be3aecad543096f92d7ea6b
frostmeta <calvin.hobo@gmail.com>
2022-02-28 14:13:37 -0500

    Passport configuration setup
    Set up passport configuration for use with the application.
    Set up a single route to return users. This is just a test

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->prefix('v1')->group(function() {
+    Route::get('/user', function(Request $request) {
+        return $request->user();
+    });
+});
+
+/* This was the default route that was here
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
